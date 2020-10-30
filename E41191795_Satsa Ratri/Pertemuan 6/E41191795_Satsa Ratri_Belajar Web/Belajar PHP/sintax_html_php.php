61696404043b0e7a097ca1876506b400c1fea8ec
SatsaRatri <71832277+SatsaRatri@users.noreply.github.com>
2020-10-30 14:30:01 +0700

    Add files via upload

---
+++ E41191795_Satsa Ratri/Pertemuan 6/E41191795_Satsa Ratri_Belajar Web/Belajar PHP/sintax_html_php.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Belajar PHP</title>
+</head>
+<body>
+	<?php  
+	echo "Saya sedang belajar PHP <br>";
+	echo "<p> Belajar PHP hingga jadi master </p>";
+	?>
+
+</body>
+</html>
---
+++ E41191795_Satsa Ratri/Pertemuan 6/E41191795_Satsa Ratri_Belajar Web/Belajar PHP/tipe_data.php
@@ -0,0 +1,28 @@
+<?php
+
+//tipe data char
+$jenis_kelamin = "L";
+
+//tipe data string
+$nama_lengkap = "Petani Kode";
+
+//tipe data integer
+$umur = 20;
+
+//tipe data float
+$berat = 48.3;
+
+//tipe data float
+$tinggi = 182.2;
+
+//tipe data boolean
+$menikah = false;
+
+echo "Nama : $nama_lengkap <br>" ;
+echo "Jenis Kelamin : $jenis_kelamin <br>";
+echo "Umur : $umur <br>";
+echo "Berat Badan : $berat Kg <br>";
+echo "Tinggi Badan : $tinggi Cm <br>";
+echo "Menikah : $menikah ";
+
+?>
