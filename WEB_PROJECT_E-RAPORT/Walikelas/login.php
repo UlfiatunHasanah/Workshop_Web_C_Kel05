22e5de6bd963c0a72b64d315f376d26b2389239b
UlfiatunHasanah <72177247+UlfiatunHasanah@users.noreply.github.com>
2021-01-13 23:16:35 +0700

    Add files via upload

---
+++ WEB_PROJECT_E-RAPORT/Walikelas/login.php
@@ -0,0 +1,35 @@
+<?php
+// memulai session
+session_start();
+require_once "library/koneksi.php";
+require_once "library/fungsi_standar.php";
+
+$wali_kelas= $_POST["wali_kelas"];
+$password = md5($_POST["password"]);
+
+
+// query untuk mendapatkan record dari siswa_id
+
+$query = "SELECT * FROM wali_kelas WHERE walikelas_id = '$wali_kelas'";
+$hasil = mysql_query($query);
+$data = mysql_fetch_array($hasil);
+
+// cek kesesuaian password
+//and($password == $data['password'])
+if (($wali_kelas == $data['walikelas_id']))
+{
+	// menyimpan walikelas_id dan level ke dalam session
+	$_SESSION['wali_kelas'] = $data['walikelas_id'];
+	$_SESSION['kelas_id'] = $data['kelas_id'];
+	$_SESSION['walikelas_nama'] = $data['walikelas_nama'];
+	
+	// tampilkan menu
+	lompat_ke("index.php");
+
+}
+else
+{
+	
+}
+
+?>
