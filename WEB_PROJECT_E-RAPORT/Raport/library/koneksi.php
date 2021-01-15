73cf3564b9a7d21a5b0b79ed930f2a8820a47ece
SatsaRatri <71832277+SatsaRatri@users.noreply.github.com>
2021-01-15 17:46:13 +0700

    Update koneksi.php

--- WEB_PROJECT_E-RAPORT/Raport/library/koneksi.php
+++ WEB_PROJECT_E-RAPORT/Raport/library/koneksi.php
@@ -7,7 +7,7 @@ $password = "";
 $database = "report_online_jur_sis";
 
 // Koneksi dan memilih database di server
-mysql_connect($server,$username,$password) or die("Koneksi gagal");
-mysql_select_db($database) or die("Database tidak bisa dibuka");
+mysqli_connect($server,$username,$password) or die("Koneksi gagal");
+mysqli_select_db($database) or die("Database tidak bisa dibuka");
 
 ?>
