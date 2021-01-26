5d744c479e584ab684f30fc85aa89fb1e1e0bde4
UlfiatunHasanah <72177247+UlfiatunHasanah@users.noreply.github.com>
2021-01-26 23:55:27 +0700

    Add files via upload

---
+++ WEB_PROJECT_E-RAPORT/Siswa/delete/data_pelajaran.php
@@ -0,0 +1,66 @@
+<?php
+require_once "library/koneksi.php";
+require_once "library/fungsi_standar.php";
+
+?>
+<a href='index.php?halaman=form_data_master&kode=pelajaran_insert' class='btn btn-large btn-info'>
+   <span>Tambah Data</span>
+   </a>
+   <br/>
+   <br/>
+<div class='row-fluid sortable'>		
+				<div class='box span12'>
+					<div class='box-header' data-original-title>
+						<h2><i class='halflings-icon user'></i><span class='break'></span>Data pelajaran</h2>
+						<div class='box-icon'>
+							<a href='#' class='btn-setting'><i class='halflings-icon wrench'></i></a>
+							<a href='#' class='btn-minimize'><i class='halflings-icon chevron-up'></i></a>
+							<a href='#' class='btn-close'><i class='halflings-icon remove'></i></a>
+						</div>
+					</div>
+					<div class='box-content'>
+						<table class='table table-striped table-bordered bootstrap-datatable datatable'>
+						  <thead>
+							  <tr>
+								  <th>No</th>
+								  <th>Kode Pelajaran</th>
+								  <th>Nama Pelajaran</th>
+								  <th>Kategori pelajaran</th>
+								  <th>Actions</th>
+							  </tr>
+						  </thead>   
+						  <tbody>
+							<tr>
+							<?php
+		$qtmpil_pelajaran="select * from pelajaran order by inc asc";						
+		$qp_brg=mysql_query($qtmpil_pelajaran);
+		$no=1;
+		while($row1=mysql_fetch_array($qp_brg)){ ?>
+		
+		
+		<td><?php echo "$no"; ?></td>
+          <td><?php echo "$row1[pelajaran_id]"; ?></td>
+          <td><?php echo "$row1[pelajaran_nama]"; ?></td>
+          <td><?php echo "$row1[pelajaran_kategori]"; ?></td>
+		  
+          <td><?php echo "<a class='btn btn-info' href=index.php?halaman=form_ubah_data&kode=pelajaran_update&id=$row1[inc]>"; ?>
+          	 <i class='halflings-icon white edit'></i>
+			 <?php echo "</a> ";
+			 
+			 echo "<a class='btn btn-danger' href='proses.php?proses=pelajaran_delete&id=$row1[inc]'>"; ?>
+          	 <i class='halflings-icon white trash'></i>
+			 <?php echo "</a>";
+
+         	 
+          echo"</td>
+		
+		
+								
+							</tr>";
+							$no++;}?>
+						  </tbody>
+					  </table>            
+					</div>
+				</div><!--/span-->
+			
+			</div><!--/row-->
---
+++ WEB_PROJECT_E-RAPORT/Siswa/delete/form_data_master.php
@@ -0,0 +1,212 @@
+<?php
+require_once "library/koneksi.php";
+require_once "library/fungsi_standar.php";
+
+	echo "
+	<form id=formID name=formInput method=post action=proses.php>
+	  <input type=hidden name=proses id=proses value=$_GET[kode] />";
+//form data pelajaran
+	if ($_GET['kode']=="pelajaran_insert"){
+		//pemanggilan fungsi penambahan
+		$a="SELECT * FROM pelajaran";
+		$b="SELECT inc FROM pelajaran ORDER BY inc DESC LIMIT 1";
+		$inc=penambahan($a, $b);
+	echo "<div class='row-fluid sortable'>
+				<div class='box span12'>
+					<div class='box-header' data-original-title>
+						<h2><i class='halflings-icon edit'></i><span class='break'></span>Form input data pelajaran</h2>
+						<div class='box-icon'>
+							<a href='#' class='btn-setting'><i class='halflings-icon wrench'></i></a>
+							<a href='#' class='btn-minimize'><i class='halflings-icon chevron-up'></i></a>
+							<a href='#' class='btn-close'><i class='halflings-icon remove'></i></a>
+						</div>
+					</div>
+					<div class='box-content'>
+						<form class='form-horizontal'>
+						  <fieldset>
+						  <input type=hidden name=inc id=inc value=$inc />
+						  
+							<div class='control-group'>
+							<label class='control-label' for='typeahead'>Kode pelajaran </label>
+							<div class='controls'>
+							
+							<input type='text' name=pelajaran_Kode class='span6 typeahead' id='typeahead' value='KODE-$inc'>
+							</div>
+							
+							<div class='control-group'>
+							<label class='control-label' for='typeahead'>Nama pelajaran </label>
+							<div class='controls'>
+							
+							<input type='text' name=nmpelajaran class='span6 typeahead' id='typeahead'>
+							</div>
+							
+							<div class='control-group'>
+							<label class='control-label' for='typeahead'>Kategori pelajaran </label>
+							<div class='controls'>
+							
+							<input type='text' name=kategori class='span6 typeahead' id='typeahead'>
+							</div>
+							
+							
+							
+							<div class='form-actions'>
+							<input type=submit class='btn btn-primary' name=SimpanBar value=Simpan />
+							<input type=reset name=BatalBar class='btn' value=Batal />
+							 
+							</div>
+						  </fieldset>
+						</form>   
+
+					</div>
+				</div><!--/span-->
+
+			</div><!--/row-->";
+	}
+	
+	elseif ($_GET['kode']=="jurusan_insert"){
+		//pemanggilan fungsi penambahan
+		$a="SELECT * FROM jurusan";
+		$b="SELECT inc FROM jurusan ORDER BY inc DESC LIMIT 1";
+		$inc=penambahan($a, $b);
+	echo "<div class='row-fluid sortable'>
+				<div class='box span12'>
+					<div class='box-header' data-original-title>
+						<h2><i class='halflings-icon edit'></i><span class='break'></span>Form input data jurusan</h2>
+						<div class='box-icon'>
+							<a href='#' class='btn-setting'><i class='halflings-icon wrench'></i></a>
+							<a href='#' class='btn-minimize'><i class='halflings-icon chevron-up'></i></a>
+							<a href='#' class='btn-close'><i class='halflings-icon remove'></i></a>
+						</div>
+					</div>
+					<div class='box-content'>
+						<form class='form-horizontal'>
+						  <fieldset>
+						  <input type=hidden name=inc id=inc value=$inc />
+						  
+							<div class='control-group'>
+							<label class='control-label' for='typeahead'>Kode jurusan </label>
+							<div class='controls'>
+							
+							<input type='text' name=jurusan_Kode class='span6 typeahead' id='typeahead' value='JUR-$inc'>
+							</div>
+							
+							<div class='control-group'>
+							<label class='control-label' for='typeahead'>Nama jurusan </label>
+							<div class='controls'>
+							
+							<input type='text' name=nmjurusan class='span6 typeahead' id='typeahead'>
+							</div>
+							
+							<div class='form-actions'>
+							<input type=submit class='btn btn-primary' name=SimpanBar value=Simpan />
+							<input type=reset name=BatalBar class='btn' value=Batal />
+							 
+							</div>
+						  </fieldset>
+						</form>   
+
+					</div>
+				</div><!--/span-->
+
+			</div><!--/row-->";
+	}
+//form data siswa
+	elseif($_GET['kode']=="siswa_insert"){
+		//pemanggilan fungsi penambahan
+		$a="SELECT * FROM siswa";
+		$b="SELECT inc FROM siswa ORDER BY inc DESC LIMIT 1";
+		$inc=penambahan($a, $b);
+    echo "<div class='row-fluid sortable'>
+				<div class='box span12'>
+					<div class='box-header' data-original-title>
+						<h2><i class='halflings-icon edit'></i><span class='break'></span>Form input data siswa </h2>
+						<div class='box-icon'>
+							<a href='#' class='btn-setting'><i class='halflings-icon wrench'></i></a>
+							<a href='#' class='btn-minimize'><i class='halflings-icon chevron-up'></i></a>
+							<a href='#' class='btn-close'><i class='halflings-icon remove'></i></a>
+						</div>
+					</div>
+					<div class='box-content'>
+						<form class='form-horizontal'>
+						  <fieldset>
+						  <input type=hidden name=siswa_inc id=siswa_inc value=$inc />
+						  
+							<div class='control-group'>
+							<label class='control-label' for='typeahead'>Kode Siswa </label>
+							<div class='controls'>
+							
+							<input type='text' name=siswa_id class='span6 typeahead' id='typeahead' value='SISWA-$inc'>
+							</div>
+							
+							<div class='control-group'>
+							<label class='control-label' for='typeahead'>Nama siswa </label>
+							<div class='controls'>
+							
+							<input type='text' name=nmsiswa class='span6 typeahead' id='typeahead'>
+							</div>
+							
+							<div class='control-group'>
+							<label class='control-label' for='typeahead'>Alamat siswa </label>
+							<div class='controls'>
+							
+							<input type='text' name=alamatSup class='span6 typeahead' id='typeahead'>
+							</div>
+							
+							<div class='control-group'>
+							<label class='control-label' for='typeahead'>Kota siswa </label>
+							<div class='controls'>
+							
+							<input type='text' name=kotaSup class='span6 typeahead' id='typeahead'>
+							</div>
+							
+							<div class='control-group'>
+							<label class='control-label' for='typeahead'>Email siswa </label>
+							<div class='controls'>
+							
+							<input type='text' name=emailSup class='span6 typeahead' id='typeahead'>
+							</div>
+							
+							<div class='control-group'>
+							<label class='control-label' for='typeahead'>Kontak siswa </label>
+							<div class='controls'>
+							
+							<input type='text' name=kontakSup class='span6 typeahead' id='typeahead'>
+							</div>
+							
+							<div class='control-group'>
+							<label class='control-label' for='typeahead'>Jurusan </label>
+							<div class='controls'>";
+							?>
+							<select name="pilih_jurusan" data-placeholder="" id="selectError2" data-rel="chosen">
+    			
+                  <?php
+						$jurusan="SELECT * FROM jurusan ORDER BY jurusan_id ASC";
+						$qjurusan=mysql_query($jurusan);
+						while($djurusan=mysql_fetch_array($qjurusan))
+						{
+							echo "<option value='$djurusan[jurusan_id]'>$djurusan[jurusan_nama]</option>";
+						}
+					?>
+  			      </select>
+							<?php
+							
+							echo"</div>
+							
+							
+							
+							<div class='form-actions'>
+							<input type=submit class='btn btn-primary' name=SimpanSup value=Simpan />
+							<input type=reset name=BatalSup class='btn' value=Batal />
+							 
+							</div>
+						  </fieldset>
+						</form>   
+
+					</div>
+				</div><!--/span-->
+
+			</div><!--/row-->";
+	}
+     echo " </form>";
+
+?>
