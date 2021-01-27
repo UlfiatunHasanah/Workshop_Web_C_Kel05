d2824e2d527e0590ce6f066957952c135c3972a0
SatsaRatri <71832277+SatsaRatri@users.noreply.github.com>
2021-01-28 06:55:39 +0700

    Update koneksi.php

--- WEB_PROJECT_E-RAPORT/Raport/library/koneksi.php
+++ WEB_PROJECT_E-RAPORT/Raport/library/koneksi.php
@@ -7,7 +7,7 @@ $password = "eraport2020";
 $database = "u1011496_report_online_jur_sis";
 
 // Koneksi dan memilih database di server
-$koneksi = mysqli_connect($server,$username,$password,$database) or die("Koneksi gagal");
+$connect = mysqli_connect($server,$username,$password,$database) or die("Koneksi gagal");
 
 
 ?>
