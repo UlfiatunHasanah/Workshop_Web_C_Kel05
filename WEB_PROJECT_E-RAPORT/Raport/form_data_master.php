a39f685b61890fec7ff3be6444d7d7c6ecdef27e
SatsaRatri <71832277+SatsaRatri@users.noreply.github.com>
2021-01-28 06:58:40 +0700

    Update form_data_master.php

--- WEB_PROJECT_E-RAPORT/Raport/form_data_master.php
+++ WEB_PROJECT_E-RAPORT/Raport/form_data_master.php
@@ -8,9 +8,10 @@ require_once "library/fungsi_standar.php";
 //form data pelajaran
 	if ($_GET['kode']=="pelajaran_insert"){
 		//pemanggilan fungsi penambahan
-		$a="SELECT * FROM pelajaran";
-		$b="SELECT inc FROM pelajaran ORDER BY inc DESC LIMIT 1";
-		$inc=penambahan($a, $b);
+		$getId = mysqli_query($connect, "SELECT count(inc) as id FROM pelajaran");
+		$data = mysqli_fetch_array($getId);
+		$lastId = $data['id'];
+		$inc = $lastId + 1;
 	echo "<div class='row-fluid sortable'>
 				<div class='box span12'>
 					<div class='box-header' data-original-title>
@@ -44,9 +45,8 @@ require_once "library/fungsi_standar.php";
 							<label class='control-label' for='typeahead'>Kategori pelajaran </label>
 							<div class='controls'>
 							
-							<input type='text' name=kategori class='span6 typeahead' id='typeahead'>
+							<input type='text' name=kategori class='span6 typeahead' id='typeahead'/>
 							</div>
-							
 							
 							
 							<div class='form-actions'>
@@ -65,9 +65,10 @@ require_once "library/fungsi_standar.php";
 	
 	elseif ($_GET['kode']=="jurusan_insert"){
 		//pemanggilan fungsi penambahan
-		$a="SELECT * FROM jurusan";
-		$b="SELECT inc FROM jurusan ORDER BY inc DESC LIMIT 1";
-		$inc=penambahan($a, $b);
+		$getId = mysqli_query($connect, "SELECT count(inc) as id FROM jurusan");
+		$data = mysqli_fetch_array($getId);
+		$lastId = $data['id'];
+		$inc = $lastId + 1;
 	echo "<div class='row-fluid sortable'>
 				<div class='box span12'>
 					<div class='box-header' data-original-title>
@@ -99,7 +100,7 @@ require_once "library/fungsi_standar.php";
 							
 							<div class='form-actions'>
 							<input type=submit class='btn btn-primary' name=SimpanBar value=Simpan />
-							<input type=reset name=BatalBar class='btn' value=Batal />
+							<a href='../raport/index.php?halaman=data_jurusan'><button type='button' name=BatalBar class='btn btn-default' value=Batal />Batal</a>
 							 
 							</div>
 						  </fieldset>
@@ -112,15 +113,16 @@ require_once "library/fungsi_standar.php";
 	}
 	elseif ($_GET['kode']=="kelas_insert"){
 		//pemanggilan fungsi penambahan
-		$a="SELECT * FROM kelas";
-		$b="SELECT inc FROM kelas ORDER BY inc DESC LIMIT 1";
-		$inc=penambahan($a, $b);
+		$getId = mysqli_query($connect, "SELECT count(inc) as id FROM kelas");
+		$data = mysqli_fetch_array($getId);
+		$lastId = $data['id'];
+		$inc = $lastId + 1;
 	echo "<div class='row-fluid sortable'>
 				<div class='box span12'>
 					<div class='box-header' data-original-title>
 						<h2><i class='halflings-icon edit'></i><span class='break'></span>Form input data kelas</h2>
 						<div class='box-icon'>
-							
+							<a href='#' class='btn-setting'><i class='halflings-icon wrench'></i></a>
 							<a href='#' class='btn-minimize'><i class='halflings-icon chevron-up'></i></a>
 							<a href='#' class='btn-close'><i class='halflings-icon remove'></i></a>
 						</div>
@@ -128,22 +130,45 @@ require_once "library/fungsi_standar.php";
 					<div class='box-content'>
 						<form class='form-horizontal'>
 						  <fieldset>
-						  <input type=hidden name=inc id=inc value=$inc />
+						  <input type=hidden name=inc id=inc value=$data[inc] />
 						  
 							<div class='control-group'>
-							<label class='control-label' for='typeahead'>Kode Kelas </label>
+							<label class='control-label' for='typeahead'>Kode kelas </label>
 							<div class='controls'>
 							
-							<input type='text' name=kelas_Kode class='span6 typeahead' id='typeahead' value='KELAS-$inc'>
+							<input type='text' readonly='yes' name=kelas_Kode class='span6 typeahead' id='typeahead' value='KELAS-$inc'/>
 							</div>
 							
 							<div class='control-group'>
-							<label class='control-label' for='typeahead'>Nama kelas </label>
+							<label class='control-label' for='typeahead'>Nama Kelas </label>
 							<div class='controls'>
 							
-							<input type='text' name=nmkelas class='span6 typeahead' id='typeahead'>
+							<input type='text' name=nmkelas class='span6 typeahead' id='typeahead' value='".$data[kelas_nama]."'/>
+							</div>
+
+							<div class='control-group'>
+							<label class='control-label' for='typeahead'>Nama Jurusan </label>
+							<div class='controls'>
+							
+							
+							<select name='nmjurusan' class='span6' typeahead'id='selectError' data-rel='chosen' value='".$data[jurusan_nama]."'>";
+					$tampil=mysqli_query($connect,"SELECT * FROM jurusan ORDER BY jurusan_nama");
+						if ($data[jurusan_id]==0){
+					echo "<option value=0 selected>- Pilih Jurusan-</option>";}   
+
+					while($w=mysqli_fetch_array($tampil)){
+					if ($data[jurusan_id]==$w[jurusan_id]){
+              echo "<option value=$w[jurusan_id] selected>$w[jurusan_nama]</option>";}
+            else{
+              echo "<option value=$w[jurusan_id]>$w[jurusan_nama]</option>";
+            }
+          }
+									
+								  echo"</select>
 							</div>
 							
+							
+
 							<div class='form-actions'>
 							<input type=submit class='btn btn-primary' name=SimpanBar value=Simpan />
 							<input type=reset name=BatalBar class='btn' value=Batal />
@@ -160,9 +185,10 @@ require_once "library/fungsi_standar.php";
 //form data siswa
 	elseif($_GET['kode']=="siswa_insert"){
 		//pemanggilan fungsi penambahan
-		$a="SELECT * FROM siswa";
-		$b="SELECT inc FROM siswa ORDER BY inc DESC LIMIT 1";
-		$inc=penambahan($a, $b);
+		$getId = mysqli_query($connect, "SELECT count(inc) as id FROM siswa");
+		$data = mysqli_fetch_array($getId);
+		$lastId = $data['id'];
+		$inc = $lastId + 1;
     echo "<div class='row-fluid sortable'>
 				<div class='box span12'>
 					<div class='box-header' data-original-title>
@@ -221,17 +247,17 @@ require_once "library/fungsi_standar.php";
 							</div>
 							
 							<div class='control-group'>
-							<label class='control-label' for='typeahead'>Jurusan </label>
+							<label class='control-label' for='typeahead'>Kelas </label>
 							<div class='controls'>";
 							?>
-							<select name="pilih_jurusan" data-placeholder="" id="selectError2" data-rel="chosen">
+							<select name="kelas_id" data-placeholder="" id="selectError2" data-rel="chosen">
     			
                   <?php
-						$jurusan="SELECT * FROM jurusan ORDER BY jurusan_id ASC";
-						$qjurusan=mysqli_query($koneksi, $jurusan);
+						$jurusan="SELECT * FROM kelas ORDER BY kelas_id ASC";
+						$qjurusan=mysqli_query($connect,$jurusan);
 						while($djurusan=mysqli_fetch_array($qjurusan))
 						{
-							echo "<option value='$djurusan[jurusan_id]'>$djurusan[jurusan_nama]</option>";
+							echo "<option value='$djurusan[kelas_id]'>$djurusan[kelas_id]</option>";
 						}
 					?>
   			      </select>
@@ -242,8 +268,8 @@ require_once "library/fungsi_standar.php";
 							
 							
 							<div class='form-actions'>
-							<input type=submit class='btn btn-primary' name=SimpanSup value=Simpan />
-							<input type=reset name=BatalSup class='btn' value=Batal />
+							<input type=submit class='btn btn-primary' name=SimpanBar value=Simpan />
+							<input type=reset name=BatalBar class='btn' value=Batal />
 							 
 							</div>
 						  </fieldset>
@@ -253,13 +279,15 @@ require_once "library/fungsi_standar.php";
 				</div><!--/span-->
 
 			</div><!--/row-->";
+	
 	}
 //form data wali kelas
 	elseif($_GET['kode']=="walikelas_insert"){
 		//pemanggilan fungsi penambahan
-		$a="SELECT * FROM wali_kelas";
-		$b="SELECT inc FROM wali_kelas ORDER BY inc DESC LIMIT 1";
-		$inc=penambahan($a, $b);
+		$getId = mysqli_query($connect, "SELECT count(inc) as id FROM wali_kelas");
+		$data = mysqli_fetch_array($getId);
+		$lastId = $data['id'];
+		$inc = $lastId + 1;
     echo "<div class='row-fluid sortable'>
 				<div class='box span12'>
 					<div class='box-header' data-original-title>
@@ -318,17 +346,17 @@ require_once "library/fungsi_standar.php";
 							</div>
 							
 							<div class='control-group'>
-							<label class='control-label' for='typeahead'>Kelas</label>
+							<label class='control-label' for='typeahead'>Kelas ID</label>
 							<div class='controls'>";
 							?>
-							<select name="pilih_kelas" data-placeholder="" id="selectError2" data-rel="chosen">
+							<select name="kelas_id" data-placeholder="" id="selectError2" data-rel="chosen">
     			
                   <?php
 						$kelas="SELECT * FROM kelas ORDER BY kelas_id ASC";
-						$qkelas=mysqli_query($koneksi, $kelas);
+						$qkelas=mysqli_query($connect,$kelas);
 						while($dkelas=mysqli_fetch_array($qkelas))
 						{
-							echo "<option value='$dkelas[kelas_id]'>$dkelas[kelas_nama]</option>";
+							echo "<option value='$dkelas[kelas_id]'>$dkelas[kelas_id]</option>";
 						}
 					?>
   			      </select>
@@ -339,8 +367,8 @@ require_once "library/fungsi_standar.php";
 							
 							
 							<div class='form-actions'>
-							<input type=submit class='btn btn-primary' name=SimpanSup value=Simpan />
-							<input type=reset name=BatalSup class='btn' value=Batal />
+							<input type=submit class='btn btn-primary' name=SimpanBar value=Simpan />
+							<input type=reset name=BatalBar class='btn' value=Batal />
 							 
 							</div>
 						  </fieldset>
