4a5f1836e95a30372aa01870ba3b09bfbfc9cbb2
SatsaRatri <71832277+SatsaRatri@users.noreply.github.com>
2021-01-28 07:10:26 +0700

    Update raport_detail2_f.php

--- WEB_PROJECT_E-RAPORT/Siswa/raport_detail2_f.php
+++ WEB_PROJECT_E-RAPORT/Siswa/raport_detail2_f.php
@@ -1,15 +1,24 @@
 <?php
+ require __DIR__.'/vendor/autoload.php';
+ use Spipu\Html2Pdf\Html2Pdf;
+ use Spipu\Html2Pdf\Exception\Html2PdfException;
+ use Spipu\Html2Pdf\Exception\ExceptionFormatter;
+ //include "raport_detail2pdf.php";
  
-ob_start();
- include "raport_detail2pdf.php";
- $content = ob_get_clean();
-
- require_once "asset/pdf/html2pdf.class.php";
+   ob_start();
+ 	include dirname(__FILE__).'\raport_detail2pdf.php';
+ 	$content = ob_get_clean();
+ //require_once "asset/pdf/html2pdf.class.php";
  try
  {
- $html2pdf = new HTML2PDF('L','A4', 'en', false, 'ISO-8859-15'); // P : Portrait or L: Landscape // page format : A4, A3, ...
- $html2pdf->writeHTML($content, isset($_GET['vuehtml']));
- $html2pdf->Output('"cetak_raport.pdf');
+ 	$html2pdf = new HTML2PDF('L','B4', 'en', false, 'ISO-8859-15'); // P : Portrait or L: Landscape // page format : A4, A3, ...
+ 	$html2pdf->writeHTML($content);
+ 	$html2pdf->Output('cetak_rapot.pdf');
+ 	ob_end_flush();
  }
- catch(HTML2PDF_exception $e) { echo $e; }
+ catch(Html2PdfException $e) {
+ 	$html2pdf->clean();
+  	$formatter = new ExceptionFormatter($e);
+  	echo $formatter->getHtmlMessage;
+}
 ?>
